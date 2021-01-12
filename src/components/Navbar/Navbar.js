7ec0e301c32d018d780e4f9f4828d6ab4474ede7
Keshav <keshavthakur.001@gmail.com>
2021-01-12 12:14:17 +0530

    Add NavMenu, NavItem, NavLinks, NavBtnLink, NavItemBtn in Navbar.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,28 @@
-import React, {useState} from 'react'
-import {Nav, NavbarContainer, NavIcon, NavLogo,MobileIcon} from './Navbar.elements'
+import React, {useState, useEffect} from 'react'
+import {Nav, NavbarContainer, NavIcon, NavLogo,MobileIcon, NavMenu, NavItem, NavLinks, NavBtnLink, NavItemBtn} from './Navbar.elements'
 import {IconContext} from 'react-icons/lib'
 import {FaBars, FaTimes} from 'react-icons/fa'
 import {Button} from "../../components/globalStyles"
 
 const Navbar = () => {
     const [click, setClick] = useState(false);
-
+    const [button, setButton] = useState(true)
     const handleClick = () => {
         setClick(!click)
     }
+    
+    const showButton = () => {
+        if(window.innerWidth <= 960)
+        setButton(false)
+        else setButton(true)
+    }
+
+    useEffect(() => {
+        showButton()
+    }, []) 
+
+    window.addEventListener('resize', showButton)
+
     return (
         <>
         <IconContext.Provider value = {{color: '#fff'}}>
@@ -22,6 +35,38 @@ const Navbar = () => {
                 <MobileIcon onClick={handleClick}>
                     {click ? <FaTimes/> : <FaBars/>}
                 </MobileIcon>
+                <NavMenu onClick={handleClick} click={click}>
+                    <NavItem>
+                        <NavLinks to="/">
+                            Home
+                        </NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to="/services">
+                            Services
+                        </NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to="/products">
+                            Products
+                        </NavLinks>
+                    </NavItem>
+                    <NavItemBtn>
+                        {button ? (
+                            <NavBtnLink to = '/sign-up'>
+                                <Button primary>
+                                    SIGN UP
+                                </Button>
+                            </NavBtnLink>
+                        ) : (
+                            <NavBtnLink to="/sign-up">
+                                <Button  fontBig primary>
+                                    SIGN UP
+                                </Button>
+                            </NavBtnLink>
+                        )}
+                    </NavItemBtn>
+                </NavMenu>       
             </NavbarContainer>
             </Nav>
             </IconContext.Provider>
